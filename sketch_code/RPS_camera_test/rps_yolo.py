2d253bdd9dcb37f0199b03b66a1f31a9bf85de28
KrissiTekkari <dadasonk@gmail.com>
2022-12-01 18:46:28 +0000

    2

---
+++ sketch_code/RPS_camera_test/rps_yolo.py
@@ -0,0 +1,100 @@
+import numpy as np
+import random
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+cap = cv.VideoCapture(1)
+
+font = cv.FONT_HERSHEY_SIMPLEX
+average_fps = 0
+e1 = cv.getTickCount()
+
+#img = cv.imread('car.jpg')
+whT = 416
+confThreshold = 0.5
+nmsThreshold = 0.3
+
+classFile = "rps.names"
+classNames = []
+with open(classFile, 'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
+
+#modelConfiguration = 'yolov3-320.cfg'
+#modelWeights = 'C:\models\yolov3.weights'
+#modelConfiguration = 'yolov3-tiny.cfg'
+#modelWeights = 'C:\models\yolov3-tiny.weights'
+
+#modelConfiguration = 'C:\models\yolov4_custom.cfg'
+#modelWeights = 'C:\models\yolov4_custom_last.weights'
+
+modelConfiguration = 'C:\models\yolov3-tiny_rps.cfg'
+modelWeights = 'C:\models\yolov3-tiny_rps_best.weights'
+
+#modelConfiguration = 'C:\models\yolov7-tiny_RPS_test.cfg'
+#modelWeights = 'C:\models\yolov7-tiny_RPS_test_last.weights'
+
+net = cv.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+
+net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
+
+def findObjects(outputs, img):
+    hT, wT, cT = img.shape
+    bbox = []
+    classIds = []
+    confs = []
+    for output in outputs:
+        for det in output:
+            scores = det[5:]
+            classId = np.argmax(scores)
+            confidence = scores[classId]
+            if confidence > confThreshold:
+                w, h = int(det[2] * wT), int(det[3] * hT)
+                x, y = int((det[0] * wT) - w / 2), int((det[1] * hT) - h / 2)
+                bbox.append([x, y, w, h])
+                classIds.append(classId)
+                confs.append(float(confidence))
+    indices = cv.dnn.NMSBoxes(bbox, confs, confThreshold, nmsThreshold)
+    for i in indices:
+        if isinstance(i, list):
+            i = i[0]
+        box = bbox[i]
+        x, y, w, h = box[0], box[1], box[2], box[3]
+        cv.rectangle(img, (x, y), (x + w, y + h), (10, 0, 255), 1)
+        cv.putText(img, f'{classNames[classIds[i]].upper()} {int(confs[i] * 100)}%',
+                   (x, y - 10), cv.FONT_HERSHEY_SIMPLEX, 1, (10, 0, 255), 2)
+
+while True:
+    success, img = cap.read()
+
+    # reduce noise
+    img = cv.GaussianBlur(img, (5, 5), 0)
+    
+    blob = cv.dnn.blobFromImage(img, 1 / 255, (whT, whT), [0, 0, 0], 1, crop=False)
+    net.setInput(blob)
+    layerNames = net.getLayerNames()
+    g = net.getUnconnectedOutLayers()
+    outputNames = [layerNames[i - 1] for i in net.getUnconnectedOutLayers()]
+    
+    outputs = net.forward(outputNames)
+
+    findObjects(outputs, img)
+    # putting the FPS count on the frame
+    time = (cv.getTickCount() - e1)/ cv.getTickFrequency()
+    e1 = cv.getTickCount()
+    fps = 1/time
+    average_fps = average_fps + (fps - average_fps)/100
+    cv.putText(img, str(np.round(fps,1)), (10, 50), font, 1, (255, 255, 0), 2, cv.LINE_AA)
+    cv.putText(img, str(np.round(average_fps,1)), (10, 100), font, 1, (125, 0, 255), 2, cv.LINE_AA)
+
+
+    cv.imshow('Image', img)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        # save the last frame
+        cv.imwrite('.\images\last_frame.jpg', img)
+        break
+
+print(blob.shape)
+
+
+
--- sketch_code/various_tests/pong_opencv.py
+++ sketch_code/various_tests/pong_opencv.py
@@ -1,35 +1,127 @@
 import numpy as np
 import cv2 as cv
 
+cap = cv.VideoCapture(1)
+
 radius = 10
 
-frame_height = 512
-frame_width = 1024
+frame_height = cap.get(cv.CAP_PROP_FRAME_HEIGHT)
+frame_width = cap.get(cv.CAP_PROP_FRAME_WIDTH)
 
 # display the frame in while loop
 # place a circle that moves and bounce off the walls
 # specify starting position and velocity
-x = 5 + radius
+x = 10 + radius
 y = 10 + radius
-vx = 2
-vy = 2
+vx = 1
+vy = 1
+
+font = cv.FONT_HERSHEY_SIMPLEX
+average_fps = 0
+e1 = cv.getTickCount()
+
+# edge detection function, when edge of circle hits canny edge, change direction
+def edge_detection(x, y, vx, vy):
+    k = 0
+    if x + radius >= frame_width:
+        vx = -vx
+        k += 1
+    if x - radius <= 0:
+        vx = -vx
+        k += 1
+    if y + radius >= frame_height:
+        vy = -vy
+        k += 1
+    if y - radius <= 0:
+        vy = -vy
+        k += 1
+    if k >= 1:
+        print('edge of frame')
+        return vx, vy
+    # if vx and vy are positive then diagonal down right
+    if vx > 0 and vy > 0:
+        # check if bottom right corner of circle is on edge
+        if edges[y + radius, x + radius] == 255:
+            # change direction
+            vx = -vx
+            #vy = -vy
+            return vx, vy
+    # if vx is positive and vy is negative then diagonal up right
+    elif vx > 0 and vy < 0:
+        # check if top right corner of circle is on edge
+        if edges[y - radius, x + radius] == 255:
+            # change direction
+            vx = -vx
+            vy = -vy
+            return vx, vy
+    # if vx is negative and vy is positive then diagonal down left
+    elif vx < 0 and vy > 0:
+        # check if bottom left corner of circle is on edge
+        if edges[y + radius, x - radius] == 255:
+            # change direction
+            vx = -vx
+            vy = -vy
+            return vx, vy
+    # if vx and vy are negative then diagonal up left
+    elif vx < 0 and vy < 0:
+        # check if top left corner of circle is on edge
+        if edges[y - radius, x - radius] == 255:
+            # change direction
+            vx = -vx
+            vy = -vy
+            return vx, vy
+    # if vx is positive and vy is 0 then right
+    elif vx > 0 and vy == 0:
+        # check if right edge of circle is on edge
+        if edges[y, x + radius] == 255:
+            # change direction
+            vx = -vx
+            return vx, vy
+    # if vx is negative and vy is 0 then left
+    elif vx < 0 and vy == 0:
+        # check if left edge of circle is on edge
+        if edges[y, x - radius] == 255:
+            # change direction
+            vx = -vx
+            return vx, vy
+    return vx, vy
+
 
 while True:
-    # clear the frame
-    frame = np.zeros((frame_height,frame_width,3), np.uint8)
+    # draw the circle on the video frame
+    ret, frame = cap.read()
+    # canny edge detection
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    gray = cv.GaussianBlur(gray, (5, 5), 0)
+    edges = cv.Canny(gray, 100, 200)
     # draw a circle
-    cv.circle(frame, (x,y), radius, (255,255,255), -1)
+    cv.circle(frame, (x,y), radius, (0,100,255), -1)
     # update the position
     x += vx
     y += vy
-    # bounce off the walls
-    if x < radius or x > frame_width - radius:
-        vx = -vx
-    if y < radius or y > frame_height - radius:
-        vy = -vy
+    # edge detection
+    vx, vy = edge_detection(x, y, vx, vy)
+
+    # also make the circle bounce off the edges
+    # if the pixel value is 255, then bounce
+    # if the pixel value is 0, then don't bounce
+    # draw line though middle of edges that has a pixel value of 255
+    # if the circle is on the line, then bounce
+    cv.line(edges, (int(frame_width/2), 0), (int(frame_width/2), int(frame_height)), (255,255,255), 1)
+
     # display the frame
+    # putting the FPS count on the frame
+    time = (cv.getTickCount() - e1)/ cv.getTickFrequency()
+    e1 = cv.getTickCount()
+    fps = 1/time
+    average_fps = average_fps + (fps - average_fps)/100
+    cv.putText(frame, str(np.round(fps,1)), (10, 50), font, 1, (255, 255, 0), 2, cv.LINE_AA)
+    cv.putText(frame, str(np.round(average_fps,1)), (10, 100), font, 1, (125, 0, 255), 2, cv.LINE_AA)
     cv.imshow('frame', frame)
+    cv.imshow('edges', edges)
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
+
+cap.release()
 cv.destroyAllWindows()
